e4ab83ca1cc551bab2f2d7342520726831e31d56
roud11 <krokozaza@gmail.com>
2023-11-19 17:59:56 +0300

    rework

---
+++ registration for a lesson.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import datetime
+import time
+
+reserve_url = 'https://mobifitness.ru/api/v6/account/reserve.json'
+
+headers = {'Authorization': 'Bearer 0de079a2b23190df8026f699b7fb8a2c'}
+stop = False
+
+while not stop:
+    if datetime.datetime.now().hour == 20:
+        zapros = requests.post(reserve_url, headers=headers, data={'fio': 'Рогачева Кристина', 'phone': '79965298428', 'scheduleId': '197679514112023', 'clubId': '1941'})
+        code = zapros.status_code
+        print(code)
+        print(zapros.content)
+        if code == 200:
+            stop = True
+    time.sleep(2)
--- try.py
+++
@@ -1,9 +0,0 @@
-from requests_html import HTMLSession
-session = HTMLSession()
-
-r = session.get('https://mobifitness.ru/widget/792082?colored=1&lines=1&club=0&clubs=1941&grid30min=0&desc=0&direction=0&group=0&trainer=0&room=0&age=&level=&activity=0&language=ru&custom_css=0&category_filter=2&activity_filter=2&ren')
-print(r)
-#r.html.render()
-#fitness-widget-club-tab-0 > div.fitness-widget-legend > span:nth-child(1)
-rasp = r.html.find('#fitness-widget-popup > div > div.fitness-widget-popup-descr-in > div.fitness-widget-popup-title', first=True)
-print(rasp.text)
